86f5bb6ec3a174065ebfefee6b1ca08ccbc1cc77
Licheng0821 <374596661@qq.com>
2023-08-15 10:08:36 +0800

    引入vant组件库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 // 导入全局的样式文件
 import './styles/index.less'
+// 加载Vant 核心组件库
+import Vant from 'vant'
+// 加载全局样式
+import 'vant/lib/index.css'
+// 注册使用vant组件库
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
